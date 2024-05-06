89ba17225046d5b0624553da20197e22a6fa9714
Arwa Tarek <118911302+rokateto@users.noreply.github.com>
2024-05-07 00:49:54 +0300

    Add files via upload

---
+++ src/finalpro/Finalpro.java
@@ -0,0 +1,18 @@
+package finalpro;
+
+import javax.swing.JFrame;
+
+public class Finalpro {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       
+        login LoginFrame = new login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+        
+    }
+    
+}
